25888127251d6a55fa0fb38230c47fddf7eafcb5
Cory Flanigan <seeflanigan@gmail.com>
2013-08-23 13:21:52 -0600

    wip

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,62 +1,76 @@
 require './item.rb'
 
 class GildedRose
+  DEFAULT_ITEMS = [
+    Item.new("+5 Dexterity Vest", 10, 20),
+    Item.new("Aged Brie", 2, 0),
+    Item.new("Elixir of the Mongoose", 5, 7),
+    Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+    Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    Item.new("Conjured Mana Cake", 3, 6)
+  ]
 
-  @items = []
+  attr_accessor :items
 
-  def initialize
-    @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+  def initialize(items=DEFAULT_ITEMS)
+    self.items = items
+  end
+
+  def appreciate!(item, num)
+    item.quality = item.quality + num
+  end
+
+  def depreciate(item, num)
+    new_value = item.quality - num
+    item.quality = new_value > 0 ? new_value : 0
+  end
+
+  def depreciate?(item)
+    ["Cake", "Elixir", "Vest"].any? { |n| item.name.match(n) }
   end
 
   def update_quality
 
-    for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1
-          end
-        end
+    items.each do |item|
+      if depreciate?(item)
+        depreciate(item, 1)
       else
-        if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].sell_in < 11)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+        if (item.quality < 50)
+          appreciate!(item, 1)
+          if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+            if (item.sell_in < 11)
+              if (item.quality < 50)
+                appreciate!(item, 1)
               end
             end
-            if (@items[i].sell_in < 6)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+            if (item.sell_in < 6)
+              if (item.quality < 50)
+                appreciate!(item, 1)
               end
             end
           end
         end
       end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1;
+      if (item.name != "Sulfuras, Hand of Ragnaros")
+        item.sell_in = item.sell_in - 1;
       end
-      if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1
+      if (item.sell_in < 0)
+        if depreciate?(item)
+          depreciate(item, 2)
+        end
+        if (item.name != "Aged Brie")
+          if (item.name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.quality > 0)
+              if (item.name != "Sulfuras, Hand of Ragnaros")
+                depreciate(item, 1)
               end
             end
           else
-            @items[i].quality = @items[i].quality - @items[i].quality
+            item.quality = item.quality - item.quality
           end
         else
-          if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
+          if (item.quality < 50)
+            appreciate!(item, 1)
           end
         end
       end
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,120 @@
 require './gilded_rose.rb'
-require "rspec"
+require 'rspec'
+require 'pry'
 
 describe GildedRose do
+  let(:brie)        { find_item("Brie") }
+  let(:cake)        { find_item("Cake") }
+  let(:elixir)      { find_item("Elixir") }
+  let(:passes)      { find_item("Backstage") }
+  let(:vest)        { find_item("Vest") }
 
-  it "should do something" do
-    subject.update_quality
+  let(:sulfuras)    { find_item("Sulfuras") }
+
+  it 'takes an array of items on initialization' do
+    items = Item.new("+5 Dexterity Vest", 10, 20)
+
+    rose = GildedRose.new(items)
+    rose.items.should eql(items)
+  end
+
+  it 'has an array of items' do
+    subject.items.should be_a(Array)
+  end
+
+  it '#depreciate reduces the quality of an item' do
+    FakeItem = Struct.new(:quality)
+    fake_item = FakeItem.new(2)
+    -> { subject.depreciate(fake_item, 1) }.
+      should change(fake_item, :quality).by(-1)
+  end
+
+  it '#depreciate is a noop when quality is zero' do
+    FakeItem = Struct.new(:quality)
+    fake_item = FakeItem.new(0)
+    -> { subject.depreciate(fake_item, 1) }.
+      should change(fake_item, :quality).by(0)
+  end
+
+  it '#appreciate! increases the quality of an item' do
+    FakeItem = Struct.new(:quality)
+    fake_item = FakeItem.new(2)
+    -> { subject.appreciate!(fake_item, 1) }.
+      should change(fake_item, :quality).by(1)
+  end
+
+  it '#depreciate? returns true for items that always depreciate' do
+    [cake, elixir, vest].each do |item|
+      subject.depreciate?(item).should be_true
+    end
   end
 
 
+  describe "characterization" do
+    it "never decreases the quality or sell_in value of Sulfuras" do
+      sulfuras.quality.should eql(80)
+      sulfuras.sell_in.should eql(0)
+
+      subject.update_quality
+
+      sulfuras.quality.should eql(80)
+      sulfuras.sell_in.should eql(0)
+    end
+
+    it "decreases the quality of items" do
+      [cake, elixir, vest].each do |item|
+        -> { subject.update_quality }.should change(item, :quality).by(-1)
+      end
+    end
+
+    it "decreases quality of items twice as fast after the sell by date" do
+      max_sell_by = items.reject { |i| i.eql?(sulfuras) }.max_by do |i|
+        i.quality
+      end.quality
+
+      (max_quality + 1).times { subject.update_quality }
+
+      [cake, elixir, passes, vest].each { |item| item.quality.should eql(0) }
+    end
+
+    it "decreases the sell_in days of all but legendary items" do
+      [brie, cake, elixir, passes, vest].each do |item|
+        -> { subject.update_quality }.should change(item, :sell_in).by(-1)
+      end
+    end
+
+    it "never decreases the quality below zero" do
+      max_quality = items.reject { |i| i.eql?(sulfuras) }.max_by do |i|
+        i.quality
+      end.quality
+
+      (max_quality + 1).times { subject.update_quality }
+
+      [cake, elixir, passes, vest].each { |item| item.quality.should eql(0) }
+    end
+
+    it "increases the quality of assets" do
+      [brie, passes].each do |item|
+        -> { subject.update_quality }.should change(item, :quality).by(1)
+      end
+    end
+  end
+
+  def find_item(name)
+    items.find { |i| i.name.match(name) }
+  end
+
+  def items
+    subject.instance_variable_get("@items")
+  end
+
 end
+
+#     it "decreases the value of conjured cake twice as fast" do
+#       cake.quality.should eql(6)
+# 
+#       subject.update_quality
+# 
+#       cake.quality.should eql(4)
+#     end
+
